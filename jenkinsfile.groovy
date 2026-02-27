562e0b44345726d7bbb5b42bfff3d84162b0a23b
AymanBOUBDIR <ay_boubdir@etu.enset-media.ac.ma>
2026-02-27 15:47:30 +0000

    change jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    environment {
+        registry = "boubdirayman/tp5projv2"
+        registryCredential = 'dockerhub-credentials'
+        dockerImage = ''
+    }
+    agent any
+    stages {
+        stage('Cloning Git') {
+            steps {
+                git branch: 'main',
+                url: 'https://github.com/AymanBOUBDIR/jenkins'
+            }
+        }
+        stage('Building image') {
+            steps {
+                script {
+                    dockerImage = docker.build registry + ":$BUILD_NUMBER"
+                }
+            }
+        }
+        stage('Test image') {
+            steps {
+                script {
+                    echo "Tests passed"
+                }
+            }
+        }
+        stage('Publish Image') {
+            steps {
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+    }
+}
